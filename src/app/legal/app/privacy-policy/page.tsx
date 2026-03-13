8c9e51e8e7ad0d6fa8e29ca41e56ff8298385d6d
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2026-03-13 11:14:33 +0200

    Landing and app legal

--- src/app/legal/app/privacy-policy/page.tsx
+++ src/app/legal/app/privacy-policy/page.tsx
@@ -13,8 +13,8 @@ export const metadata: Metadata = {
     },
 
     robots: {
-        index: true,
-        follow: true,
+        index: false,
+        follow: false,
     },
 
     appleWebApp: {
